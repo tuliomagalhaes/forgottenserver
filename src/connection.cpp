59d9353226568a7abee45e771c797e794611fda8
Damian Jarek <damian.jarek93@gmail.com>
2015-08-13 01:57:39 +0200

    Remove io_service reference from Connection.
    A reference can already be obtained from the socket object.

--- src/connection.cpp
+++ src/connection.cpp
@@ -287,7 +287,7 @@ void Connection::dispatchBroadcastMessage(const OutputMessage_ptr& msg)
 {
 	auto msgCopy = OutputMessagePool::getOutputMessage();
 	msgCopy->append(msg);
-	io_service.dispatch(std::bind(&Connection::broadcastMessage, shared_from_this(), msgCopy));
+	socket.get_io_service().dispatch(std::bind(&Connection::broadcastMessage, shared_from_this(), msgCopy));
 }
 
 void Connection::broadcastMessage(OutputMessage_ptr msg)
